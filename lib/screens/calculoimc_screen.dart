6ac814998e66a6895a2adfc591b3f6ae25361e18
Juscimara <juscimara.cb@gmail.com>
2021-04-08 22:31:01 -0300

    aula_08/04

---
+++ lib/screens/calculoimc_screen.dart
@@ -0,0 +1,15 @@
+import 'package:app_imc/widgets/calculoimc_widget.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CalculoImcScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Calculo IMC"),
+      ),
+      body: CalculoImcWidget(),
+    );
+  }
+}
